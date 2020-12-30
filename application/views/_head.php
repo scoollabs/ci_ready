00652350a42c70d5c8b7c1aa2049f0d0aed5cdd1
Ian Escarro <ian.escarro@gmail.com>
2020-12-30 11:55:32 +0800

    Add base url and other styles

--- application/views/_head.php
+++ application/views/_head.php
@@ -1,4 +1,5 @@
 <head>
+  <base href="<?php echo base_url(); ?>">
 
   <!-- jQuery -->
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -8,7 +9,10 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 
-  <link rel="stylesheet" href="themes/default/css/style.css">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
+  <!-- Custom -->
+  <link rel="stylesheet" href="themes/default/css/style.css">
 
 </head>
