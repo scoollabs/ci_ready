7fe8ecaad474e1f9bea85647775d8aee0d532976
Ian Escarro <ian.escarro@gmail.com>
2022-07-18 10:34:48 +0800

    Add more formats

--- application/helpers/date_helper.php
+++ application/helpers/date_helper.php
@@ -1,10 +1,16 @@
 <?php
 
-define('DEFAULT_DATE_FORMAT', 'Y-m-d H:i:s');
+define('DEFAULT_DATETIME_FORMAT', 'Y-m-d H:i:s');
+define('DEFAULT_DATE_FORMAT', 'Y-m-d');
+define('READABLE_DATETIME_FORMAT', 'M,d Y g:i A');
 
-function now() {
+function now($format = DEFAULT_DATETIME_FORMAT) {
   $date = new DateTime();
-  return $date->format(DEFAULT_DATE_FORMAT);
+  return $date->format($format);
+}
+
+function yesterday() {
+  return date(DEFAULT_DATETIME_FORMAT, strtotime('-1 days', strtotime(now())));
 }
 
 function month() {
